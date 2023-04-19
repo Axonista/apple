ac8ccef3f038b9267019aa83a28aa6ea0c6a6987
Sergey Mingalev <sergey.mingalev@axonista.com>
2023-04-19 11:08:52 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,29 +1,22 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// https://developer.apple.com/documentation/xcode/creating_a_standalone_swift_package_with_xcode?language=objc
 import PackageDescription
 
-let package = Package(
-    name: "RemocoSDK",
+let BrightcovePlayerSDK = Package(
+    name: "Remoco SDK",
+    platforms: [
+        .iOS(.v12), .tvOS(.v12)
+    ],
     products: [
         .library(
-            name: "RemocoSDK",
-            targets: [
-                "RemocoSDKBinary"
-            ]
-        )
+            name: "Remoco SDK",
+            targets: ["RemocoSDK"])
     ],
     targets: [
-        .target(
-            name: "RemocoSDK",
-            dependencies: []),
         .binaryTarget(
-            name: "RemocoSDKBinary",
-            url: "https://github.com/Axonista/apple-sdk/releases/download/refs%2Fheads%2Ffeature%2Faction/apple-sdk.xcframework.zip",
-            checksum: "9ddec41f98fb31637d2a651e77991025158efeb9d6be2c074fd7200b80f7910f"
-        ),
-        .testTarget(
-            name: "RemocoSDKTests",
-            dependencies: ["RemocoSDK"]),
+                name: "RemocoSDK",
+                path: "RemocoSDK.xcframework"
+            )
     ]
 )
